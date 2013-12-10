64ac03dfb2a9d59c180445fd0851aa8682bbeaf3
gq <gq@gq-ubuntu.(none)>
2013-12-10 18:30:38 +0800

    4

--- as.py
+++ as.py
@@ -3,15 +3,16 @@ from numpy import *
 from RGBtol import *
 from ltoRGB import *
 from change import *
-filename_s = '/home/gq/Desktop/dog.jpg'
-filename_d = '/home/gq/Desktop/tree.jpg'
+filename_s = '/home/gq/Desktop/car1.jpg'
+filename_t = '/home/gq/Desktop/car2.jpg'
 img_s= cv2.imread(filename_s)
-img_d=cv2.imread(filename_d)
+img_t=cv2.imread(filename_t)
+
 l_s=RGBtol(img_s)
-l_d=RGBtol(img_d)
-l_d=change(l_s,l_d)
-RGB=ltoRGB(l_d)
-zz=array(RGB.reshape(img_d.shape))
+l_t=RGBtol(img_t)
+l_s=change(l_s,l_t)
+RGB=ltoRGB(l_s)
+zz=array(RGB.reshape(img_t.shape))
 zz=zz.astype('uint8')
 cv2.imshow('f',zz)
 cv2.waitKey(0)
--- change.py
+++ change.py
@@ -1,10 +1,16 @@
 import cv2
 from numpy import *
-def change(l_s,l_d):
+def change(l_s,l_t):
     row,N=l_s.shape
     ave_s=average(l_s.T,0)
-    stdd_s=std(l_s.T,0)
-    for i in range(1,N):
-        l_s[:,i]=l_s[:,i]-ave_s;
-    print N 
-    return l_d
+    ave_t=average(l_t.T,0)
+    std_s=std(l_s.T,0)
+    std_t=std(l_t.T,0)
+    l_s=l_s-ave_s[:,None]
+    trans=(std_t/std_s)
+    trans=array([[trans[0],0,0],
+                 [0,trans[1],0],
+                 [0,0,trans[2]]])
+    l_s=dot(trans,l_s)
+    l_s=l_s+ave_t[:,None]
+    return l_s
